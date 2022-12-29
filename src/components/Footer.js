783c7c35bedfca285db9fba438b823633295b97a
Sitanshu-Lingwal <slslsingh17@gmail.com>
2022-12-29 11:24:46 +0530

    project1

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechEE</h1>
+        <p>@All rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@sitanshulingwal9436" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/lingwalsitanshu" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Sitanshu-Lingwal-au45" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
